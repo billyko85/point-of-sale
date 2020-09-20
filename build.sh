c0d7de589e435bba22517efe67a130e6fafe7ee4
Mauro Pampin <mauro.pampin@redb.ee>
2020-09-20 18:18:21 -0300

    Creo workflow para deploy

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ PACKAGE_VERSION=$(cat package.json \
   | head -1 \
   | awk -F: '{ print $2 }' \
   | sed 's/[", ]//g')
-echo "sdf$PACKAGE_VERSION"
+
 docker build -t "chapadany.azurecr.io/point-of-sale-api:$PACKAGE_VERSION" .
 
 
