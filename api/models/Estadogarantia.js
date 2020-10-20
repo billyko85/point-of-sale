2aeed310a15438ea8881a71fc91f950caccc7d80
mpampin <mauro.pampin@gmail.com>
2020-10-20 20:14:51 -0300

    Estadogarantia missing code

--- api/models/Estadogarantia.js
+++ api/models/Estadogarantia.js
@@ -29,16 +29,19 @@ module.exports = {
       allowNull: true
     }
 
-    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
-    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
-    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
-
-
-    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
-    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
-    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
-
   },
+  
+  beforeCreate: async (values, cb) => {
+
+    const estados = await Estadogarantia.find({ garantia_id: values.garantia_id })
+    const secuencia = reduce(estados, (max, e) => e.secuencia > max ? e.secuencia + 1 : max, 0)
+    
+    values.secuencia = secuencia + 1
+    values.fecha = new Date()
+
+    cb()
+
+  }
 
 };
 
